8294210ff3a591062536ccc98c372d921148e97a
Jesus Jaraba <jjaraba@dexfreight.io>
2020-03-09 09:13:49 -0500

    (FIX)

--- src/app/components/tortas/tortas.component.ts
+++ src/app/components/tortas/tortas.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from 'src/app/servicios/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import Swal from 'sweetalert2'
+// import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-tortas',
@@ -20,25 +20,25 @@ tortas;
   });
   }
 
-  async verTortas(producto, index){
-    console.log(producto)
-    const { value: email } = await Swal.fire({
-      title: 'Confirmacion Orden',
-      text: "Por favor llena tu email y te enviaremos una cotizacion",
-      icon: 'question',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Ordenar',
-      input: 'email',
-      inputPlaceholder: 'Ingresa tu correo '
-    })
-    if (email) {
-      Swal.fire(`En unos minutos encontraras un resumen y confirmacion de tu orden en tu correo`)
-    }
-    if(index === 0){
-      localStorage.exist = true;
-      this.router.navigate( ['/about'] );
-    }
-  }
+  // async verTortas(producto, index){
+  //   console.log(producto)
+  //   const { value: email } = await Swal.fire({
+  //     title: 'Confirmacion Orden',
+  //     text: "Por favor llena tu email y te enviaremos una cotizacion",
+  //     icon: 'question',
+  //     showCancelButton: true,
+  //     confirmButtonColor: '#3085d6',
+  //     cancelButtonColor: '#d33',
+  //     confirmButtonText: 'Ordenar',
+  //     input: 'email',
+  //     inputPlaceholder: 'Ingresa tu correo '
+  //   })
+  //   if (email) {
+  //     Swal.fire(`En unos minutos encontraras un resumen y confirmacion de tu orden en tu correo`)
+  //   }
+  //   if(index === 0){
+  //     localStorage.exist = true;
+  //     this.router.navigate( ['/about'] );
+  //   }
+  // }
 }
